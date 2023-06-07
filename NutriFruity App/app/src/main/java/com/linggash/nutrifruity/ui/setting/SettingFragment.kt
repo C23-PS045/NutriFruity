f38ca96e80b42fd9f85b2f05834c7c8865b3b6c7
linggash <sempanalingga@gmail.com>
2023-06-07 23:31:11 +0700

    refoactoring some code and change color

--- NutriFruity App/app/src/main/java/com/linggash/nutrifruity/ui/setting/SettingFragment.kt
+++ NutriFruity App/app/src/main/java/com/linggash/nutrifruity/ui/setting/SettingFragment.kt
@@ -1,4 +1,4 @@
-package com.linggash.nutrifruity.ui.dashboard
+package com.linggash.nutrifruity.ui.setting
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.linggash.nutrifruity.databinding.FragmentDashboardBinding
+import com.linggash.nutrifruity.databinding.FragmentSettingBinding
 
-class DashboardFragment : Fragment() {
+class SettingFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentSettingBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,14 +20,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val settingViewModel =
+            ViewModelProvider(this).get(SettingViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        settingViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- NutriFruity App/app/src/main/java/com/linggash/nutrifruity/ui/setting/SettingViewModel.kt
+++ NutriFruity App/app/src/main/java/com/linggash/nutrifruity/ui/setting/SettingViewModel.kt
@@ -1,13 +1,13 @@
-package com.linggash.nutrifruity.ui.notifications
+package com.linggash.nutrifruity.ui.setting
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SettingViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
