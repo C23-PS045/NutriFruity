05c7bb91e2d609d60523303566cc4d0df74ab4f9
linggash <sempanalingga@gmail.com>
2023-06-08 13:29:22 +0700

    setting page finish

--- NutriFruity App/app/src/main/java/com/linggash/nutrifruity/ui/setting/SettingFragment.kt
+++ NutriFruity App/app/src/main/java/com/linggash/nutrifruity/ui/setting/SettingFragment.kt
@@ -5,9 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.linggash.nutrifruity.R
 import com.linggash.nutrifruity.databinding.FragmentSettingBinding
+import com.linggash.nutrifruity.ui.custom.CardComponent
+import com.linggash.nutrifruity.ui.theme.PetitCochon
 
 class SettingFragment : Fragment() {
 
@@ -20,15 +34,51 @@ class SettingFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val settingViewModel =
-            ViewModelProvider(this).get(SettingViewModel::class.java)
-
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        settingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.cvTitle.setContent {
+            CardComponent(
+                borderColor = colorResource(R.color.orange_secondary),
+                cardColor = colorResource( R.color.orange_primary) ,
+                cardShape = CircleShape,
+                borderSize = 5.dp
+            ) {
+                Text(
+                    text = getString(R.string.setting),
+                    color = Color.White,
+                    fontFamily = PetitCochon,
+                    fontSize = 48.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+        binding.switchMusic.setContent {
+            Switch(
+                colors = SwitchDefaults.colors(
+                    checkedBorderColor = colorResource( R.color.orange_primary),
+                    checkedThumbColor = colorResource( R.color.orange_third),
+                    checkedTrackColor = colorResource( R.color.orange_primary),
+                ),
+                checked = true,
+                onCheckedChange ={
+
+                }
+            )
+        }
+        binding.switchSound.setContent {
+            Switch(
+                colors = SwitchDefaults.colors(
+                    checkedBorderColor = colorResource( R.color.orange_primary),
+                    checkedThumbColor = colorResource( R.color.orange_third),
+                    checkedTrackColor = colorResource( R.color.orange_primary),
+                ),
+                checked = true,
+                onCheckedChange ={
+
+                }
+            )
         }
         return root
     }
