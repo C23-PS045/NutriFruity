d43df742ff9c36c9622ee371cc34f21c6ea45b68
linggash <sempanalingga@gmail.com>
2023-06-11 11:33:49 +0700

    added splash screen

--- NutriFruity App/app/src/main/java/com/linggash/nutrifruity/MainActivity.kt
+++ NutriFruity App/app/src/main/java/com/linggash/nutrifruity/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.linggash.nutrifruity
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.linggash.nutrifruity.databinding.ActivityMainBinding
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         val navView: BottomNavigationView = binding.navView
 
